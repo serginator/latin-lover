2c78d4898eea085703529e36bc4fcda9c492c27a
Carlos Hernández Gómez <karliky@gmail.com>
2012-08-12 13:03:33 +0200

    Subida de SlimFramwork a /rest y actualización de tareas

--- README.md
+++ README.md
@@ -7,4 +7,13 @@ TODO
 ===========
 - Clase de comunicación con la API de Twitter
 - Registro de frases, posibilidad de elegir entre "Románticas" y "Graciosas"
+- En /rest está instalado SlimFramework, será el servicio que utilicemos desde el app móvil.
+- Crear un documento de tareas para cada persona.
 .....
+TODO
+===========
+Deberíamos definir la estructura de las frases, he pensado en algo así:
+```
+Mi querida {{twitter_name}}, desde esta apartada orilla, mirando las estrellas, sólo se me ocurre, tocarte las mamellas.
+``
+Siendo {{twitter_name}} el usuario de Twitter al que se le quiere mencionar.
---
+++ db.sql
@@ -0,0 +1,20 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`lover_phrases`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `lover_phrases` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `phrase` VARCHAR(140) NULL ,
+  `date_created` TIMESTAMP NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
